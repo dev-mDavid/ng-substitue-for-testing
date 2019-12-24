fa4fa2c5e56c82fee95a653a123f184555433658
mDavid <dev.mdavid@gmail.com>
2019-12-24 13:54:27 -0800

    config RealSource to generate a random array of numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RealSource } from './real-source';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-substitute-for-testing';
+
+
 }
+console.log(
+  RealSource.prototype.randomData()
+);
--- src/app/real-source.ts
+++ src/app/real-source.ts
@@ -1,2 +1,23 @@
 export class RealSource {
+
+  randomData() {
+    let randomNum1To10 = Math.floor(Math.random() * 10) 
+    function randomNum1To100() {
+      return Math.floor(Math.random() * 100) 
+    } 
+
+    let arrayAmount = randomNum1To10;
+    let randomArray = [];
+    // let randomNum = new randomNum1To100();
+    
+    for (let index = 0; index < arrayAmount; index++) {
+      randomArray.push(randomNum1To100());
+    }
+
+    return randomArray;
+
+  }
+  // randomData = [1, 2];
+
+   
 }
