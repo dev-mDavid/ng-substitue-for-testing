11689e957e17953bc8bc371edd1e430523ff5a76
mDavid <dev.mdavid@gmail.com>
2019-12-24 14:01:11 -0800

    generate CollectNumsModel

---
+++ src/app/collect-nums-model.spec.ts
@@ -0,0 +1,7 @@
+import { CollectNumsModel } from './collect-nums-model';
+
+describe('CollectNumsModel', () => {
+  it('should create an instance', () => {
+    expect(new CollectNumsModel()).toBeTruthy();
+  });
+});
